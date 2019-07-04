0b35824b8abcfcfeb406a7c24e60e820fad26f57
Muhammed Ashiq <ashiqm@amazon.com>
2019-07-04 11:29:20 +0530

    Updates

---
+++ precache-manifest.e7c2b1be611aa2cccb09d3934743552e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e2c0b5ab0f8c092d2bc082912342ae",
+    "url": "/test-screen-poc/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/test-screen-poc/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "955a96587437512257f2",
+    "url": "/test-screen-poc/static/js/main.305b6859.chunk.js"
+  },
+  {
+    "revision": "ea16af01669d69ab5460",
+    "url": "/test-screen-poc/static/js/runtime~main.77b3e895.js"
+  }
+]);
